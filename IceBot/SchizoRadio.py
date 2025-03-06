dfe3011401f5095002a949d9e81063fee4371c0c
LlessReal <marshall3001.mm@gmail.com>
2025-03-06 16:19:37 -0500

    Playlist now updates in real time

--- IceBot/SchizoRadio.py
+++ IceBot/SchizoRadio.py
@@ -2,14 +2,12 @@ import os, pygame, config, random, threading, time
 #pygame.mixer.init() # For debugging
 #pygame.mixer.set_num_channels(22)
 
-num_channels = pygame.mixer.get_num_channels()
-print(f"Number of available channels: {num_channels}")
-MusicPlaylist = os.listdir(f"{config.CurrentPath}\\Wait Music Playlist")
-channel21 = pygame.mixer.Channel(21)
-channel21.set_volume(0) # Initially radio is off
+num_channels = pygame.mixer.get_num_channels(); print(f"Number of available channels: {num_channels}")
+channel21 = pygame.mixer.Channel(21); channel21.set_volume(0) # Initially radio is off
 
 def MusicChanging():
     while True: # Loop that changes song when song stops
+        MusicPlaylist = os.listdir(f"{config.CurrentPath}\\Wait Music Playlist")
         MusicToBePlayed = pygame.mixer.Sound(f"{config.CurrentPath}\\Wait Music Playlist\\{random.choice(MusicPlaylist)}")
         channel21.play(MusicToBePlayed)
         while channel21.get_busy(): pygame.time.wait(100)  # Wait for 100 milliseconds to avoid busy-waiting
